29d7d8fcf8915ae4385c935771eb760e277799e2
RohanS2003 <118051784+RohanS2003@users.noreply.github.com>
2026-05-29 23:43:57 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+
+        int[] dp = new int[n+1];
+         dp[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+
+                if (i + 1 < n) {
+                    int value = Integer.parseInt(s.substring(i, i + 2));
+
+                    if (value >= 10 && value <= 26) {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    private int solve(int i, String s, int[] dp) {
+        if (i == s.length()) {
+            return 1;
+        }
+
+        if (s.charAt(i) == '0') {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int oneDigit = solve(i + 1, s, dp);
+
+        int twoDigit = 0;
+
+        if (i + 1 < s.length()) {
+            int value = Integer.parseInt(s.substring(i, i + 2));
+
+            if (value >= 10 && value <= 26) {
+                twoDigit = solve(i + 2, s, dp);
+            }
+        }
+
+        dp[i] = oneDigit + twoDigit;
+
+        return dp[i];
+    }
+}
