6141ecf8e3ce641dd8709ddc6a3a6d07eacc2181
RohanS2003 <118051784+RohanS2003@users.noreply.github.com>
2026-06-21 16:36:08 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set=new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+
+        dp[n] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int end = i; end < n; end++) {
+                String str = s.substring(i, end + 1);
+
+                if (set.contains(str) && dp[end + 1]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    private boolean solve(String s, int i, Set<String> set, int[] dp){
+        int n=s.length();
+        if(i==n)
+        return true;
+
+        if (dp[i] != -1) {
+            return dp[i] == 1;
+        }
+
+        for(int end=i;end<n;end++){
+            String str=s.substring(i,end+1);
+            if(set.contains(str)){
+                if(solve(s,end+1,set,dp)){
+                    dp[i]=1;
+                    return true;
+                }
+            }
+        }
+        dp[i]=0;
+        return false;
+    }
+}
