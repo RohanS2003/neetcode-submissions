c76c8ef6f4320d61672b772a28f7adbab8f599a3
RohanS2003 <118051784+RohanS2003@users.noreply.github.com>
2026-06-21 19:20:40 +0530

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        //int[][] dp = new int[m+1][n+1];
+
+        // for (int[] row : dp) {
+        //     Arrays.fill(row, -1);
+        // }
+
+        int[] next=new int[n+1];
+
+        //return solve(0,0,text1,text2,dp);
+        for(int i=m-1;i>=0;i--){
+            int[] curr=new int[n+1];
+            for(int j=n-1;j>=0;j--){
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    curr[j] = 1 + next[j + 1];
+                } else {
+                    curr[j] = Math.max(next[j], curr[j + 1]);
+                }
+            }
+            next=curr;
+        }
+        return next[0];
+    }
+
+    private int solve(int i, int j, String text1, String text2, int[][] dp){
+        if(i==text1.length() || j==text2.length())
+        return 0;
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if(text1.charAt(i)==text2.charAt(j)){
+            dp[i][j]=1+solve(i+1,j+1,text1,text2,dp);
+        }else{
+            int skipText1 = solve(i + 1, j, text1, text2,dp);
+            int skipText2 = solve(i, j + 1, text1, text2, dp);
+
+            dp[i][j]=Math.max(skipText1, skipText2);
+        }
+
+        
+
+        return dp[i][j];
+    }
+}
