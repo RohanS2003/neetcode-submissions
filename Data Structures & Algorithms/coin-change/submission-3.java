ebe9e72eb480b1ef40cd3351900a094ab98fc6f2
RohanS2003 <118051784+RohanS2003@users.noreply.github.com>
2026-06-26 14:58:13 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        //return solve(coins, 0, amount);
+        int INF=amount+1;
+        int[] dp=new int [amount+1];
+        Arrays.fill(dp,INF);
+
+        dp[0]=0;
+        for(int amt=1;amt<=amount;amt++){
+            for(int coin:coins){
+                if(amt-coin>=0){
+                    dp[amt]=Math.min(dp[amt], 1+dp[amt-coin]);
+                }
+            }
+        }
+        return dp[amount]==INF?-1:dp[amount];
+    }
+}
